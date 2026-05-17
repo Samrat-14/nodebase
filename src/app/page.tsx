970d95df49eba83a9422f96de466e2b1a6f6fdf8
Samrat-14 <samratdewdrops0@gmail.com>
2026-05-17 10:54:19 +0530

    feat(setup): prisma, neondb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Hello world</div>;
+import prisma from '@/lib/db';
+
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
+    </div>
+  );
 }
