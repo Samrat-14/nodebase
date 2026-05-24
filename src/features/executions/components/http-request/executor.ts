232dd75077a86f00f28b793c870252f753e26ffe
Samrat-14 <samratdewdrops0@gmail.com>
2026-05-24 06:50:13 +0530

    feat(ui): add node templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,19 @@
 import { NonRetriableError } from 'inngest';
 import ky, { type Options as KyOptions } from 'ky';
+import Handlebars from 'handlebars';
 import type { NodeExecutor } from '@/features/executions/types';
 
+Handlebars.registerHelper('json', (context) => {
+  const jsonString = JSON.stringify(context, null, 2);
+  const safeString = new Handlebars.SafeString(jsonString);
+
+  return safeString;
+});
+
 type HttpRequestData = {
-  variableName?: string;
-  endpoint?: string;
-  method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  variableName: string;
+  endpoint: string;
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
   body?: string;
 };
 
@@ -23,14 +31,20 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     throw new NonRetriableError('HTTP Request node: No endpoint configured');
   }
 
+  if (!data.method) {
+    throw new NonRetriableError('HTTP Request node: No method configured');
+  }
+
   const result = await step.run('http-request', async () => {
-    const endpoint = data.endpoint!;
+    const endpoint = Handlebars.compile(data.endpoint)(context);
     const method = data.method || 'GET';
 
     const options: KyOptions = { method };
 
     if (['POST', 'PUT', 'PATCH'].includes(method)) {
-      options.body = data.body;
+      const resolved = Handlebars.compile(data.body || '{}')(context);
+      JSON.parse(resolved);
+      options.body = resolved;
       options.headers = {
         'Content-Type': 'application/json',
       };
@@ -52,7 +66,7 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
 
     return {
       ...context,
-      [data.variableName!]: responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
 
